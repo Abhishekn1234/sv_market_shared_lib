f48eef896edf647d52f03b1c310143976d37548e
Abhishek N <abhishekpes123@gmail.com>
2026-02-09 15:13:18 +0530

    makes changes in language schema

--- src/language/schema/language.schema.ts
+++ src/language/schema/language.schema.ts
@@ -3,8 +3,7 @@ import { Prop,Schema,SchemaFactory } from "@nestjs/mongoose";
 
 @Schema({ timestamps: true })
 export class Language {
-  @Prop({ required: true })
-  mobile: string;
+
 
   @Prop({
     type: String,
